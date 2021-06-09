a0781cf967e06be3d4ae4eb231d3feefdd18098c
Clayton Degallerie <cod5000@gmail.com>
2021-06-09 16:41:18 -0400

    max sum in array

---
+++ arrayMax.js
@@ -0,0 +1,16 @@
+function arrayMax(array) {
+    let max = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (max < array[i] + array[i + 1]) {
+            max = array[i] + array[i + 1]
+        }
+    }
+
+
+    return "hello" + max;
+}
+
+console.log(arrayMax([2, 3, 5, 1, 9]))
+
+//find max sum of consecutive elements in array
